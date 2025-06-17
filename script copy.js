dfbad49b43567d91d79d8e78f1811d1025b53c1e
Marcelo <mcomanduci@gmail.com>
2025-06-17 09:41:32 -0300

    first commit

--- script copy.js
+++ script copy.js
@@ -1691,7 +1691,16 @@ window.saveReciboConfig = async () => {
 // --- FIREBASE INITIALIZATION ---
 
 try {
-  app = initializeApp();
+  const fbConfig = {
+    apiKey: __API_KEY__,
+    authDomain: __AUTH_DOMAIN__,
+    projectId: __PROJECT_ID__,
+    storageBucket: __STORAGE_BUCKET__,
+    messagingSenderId: __MESSAGING_SENDER_ID__,
+    appId: __APP_ID__,
+  };
+
+  app = initializeApp(fbConfig);
   db = getFirestore(app);
   auth = getAuth(app);
   appId = "solares-app-default";
