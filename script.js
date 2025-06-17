776285255bcf2bd499b13c45a56586edb23e5039
Marcelo <mcomanduci@gmail.com>
2025-06-17 09:56:16 -0300

    first commit

--- script.js
+++ script.js
@@ -1691,7 +1691,16 @@ window.saveReciboConfig = async () => {
 // --- FIREBASE INITIALIZATION ---
 
 try {
-  app = initializeApp();
+  const fbConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+  app = initializeApp(fbConfig);
   db = getFirestore(app);
   auth = getAuth(app);
   appId = "solares-app-default";
