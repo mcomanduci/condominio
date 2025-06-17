e8b18999b18d929f0f498ae1aeb74c8e0a7ad541
Marcelo <mcomanduci@gmail.com>
2025-06-17 08:48:26 -0300

    first commit

--- script.js
+++ script.js
@@ -1691,17 +1691,19 @@ window.saveReciboConfig = async () => {
 // --- FIREBASE INITIALIZATION ---
 
 try {
-  fetch("./fbConfig.json")
-    .then((response) => response.json())
-    .then((config) => {
-      const app = initializeApp(config);
-      const db = getFirestore(app);
-      const auth = getAuth(app);
-      // ...
-    })
-    .catch((err) => {
-      console.error("Erro ao carregar o fbConfig.json", err);
-    });
+  const fbConfig = {
+    apiKey: __FIREBASE_API_KEY__,
+    authDomain: __FIREBASE_AUTH_DOMAIN__,
+    projectId: __FIREBASE_PROJECT_ID__,
+    storageBucket: __FIREBASE_STORAGE_BUCKET__,
+    messagingSenderId: __FIREBASE_MESSAGING_SENDER_ID__,
+    appId: __FIREBASE_APP_ID__,
+  };
+
+  app = initializeApp(fbConfig);
+  db = getFirestore(app);
+  auth = getAuth(app);
+  appId = "solares-app-default";
 
   onAuthStateChanged(auth, async (user) => {
     if (user) {
